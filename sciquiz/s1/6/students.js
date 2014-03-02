9accc182282952bfe8e17b7adca416e643397346
Ujamshi Khandla <ucksocial@gmail.com>
2014-03-02 12:41:48 +0530

    Create students.js

---
+++ sciquiz/s1/6/students.js
@@ -0,0 +1,22 @@
+var studentsJSON = {
+	"students": [
+		{
+			"std": "6",
+			"name": "વિદ્યાર્થીનું પુરૂ નામ",
+			"rollnumber": "1",
+			"grnumber": "1"
+		},
+		{
+			"name": "વિદ્યાર્થીનું પુરૂ નામ",
+			"rollnumber": "1",
+			"grnumber": "1"
+		},
+		{
+			"std": "8",
+		  "name": "વિદ્યાર્થીનું પુરૂ નામ",
+			"rollnumber": "1",
+			"grnumber": "1"
+		}
+	]
+
+};
